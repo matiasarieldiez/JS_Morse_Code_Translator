a64eba693bd208528d250985d6c7a3731cf09fe3
Matias Ariel Diez <matiasarieldiez@gmail.com>
2022-07-21 08:59:15 +1000

    Making the JS Script modular

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+import {
+    englishToMorseCodeButton,
+    morseCodeToEnglishButton,
+    morseText,
+    englishText,
+} from "./modules/DOM_Utilities.js";
+import { englishToMorse } from "./modules/englishToMorseCode.js";
+import { morseToEnglish } from "./modules/morseCodeToEnglish.js";
+
+englishToMorseCodeButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const textToTranslate = englishText.value;
+    morseText.value = englishToMorse(textToTranslate);
+});
+
+morseCodeToEnglishButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const textToTranslate = morseText.value;
+    englishText.value = morseToEnglish(textToTranslate);
+});
